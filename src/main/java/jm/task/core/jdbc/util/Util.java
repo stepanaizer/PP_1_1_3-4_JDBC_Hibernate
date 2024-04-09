f991ceffb76d8d4d868c486e2bfe918aede06457
Stepan <stpn.pwn@gmail.com>
2024-04-09 21:08:38 +0300

    Исправил: создание Connection в классе Util, методы в классе UserDaoJDBCImpl, объявление объекта ДАО на уровне интерфейса в сервисе.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Util {
     // реализуйте настройку соеденения с БД
